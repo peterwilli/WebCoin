a4e6145cd5b0dfb424c166922b0623ec67bda352
Peter Willemsen <peter@codebuffet.co>
2017-06-11 11:20:25 +0200

    signature finished

--- web-coin-wallet/src/server/main.js
+++ web-coin-wallet/src/server/main.js
@@ -1,4 +1,16 @@
 module.exports = {
+  peers: [],
+  addPeer(peer) {
+    var conn = peer.connect(peer)
+    this.peers.push({
+      conn, id: peer
+    })
+  },
+  broadcast(msg) {
+    for(var peer in this.peers) {
+      peer.conn.send(msg)
+    }
+  },
   start() {
     var peer = new Peer({ key: '4rvj8mvhtbq8semi' })
     peer.on('open', (id) => {
--- web-coin-wallet/src/wallet/payment.js
+++ web-coin-wallet/src/wallet/payment.js
@@ -1,9 +1,12 @@
 const clock = require("@/server/clock")
 const crypto = require('crypto')
+const server = require("@/server/main")
 
 module.exports = (wallet, to, amount) => {
   // Create the transaction and the hash of it.
   var transaction = `${to}:${amount}:${clock.ts()}`
   var hash = crypto.createHash('sha256').update(transaction).digest('hex');
-  console.log(hash);
+  var signature = new Buffer(wallet.key.sign(hash).toDER()).toString("hex");
+  var packet = `${transaction}|${signature}`
+  server.broadcast(packet)
 }
